5311d8a24be6fd73f6241589a0eee043111ffe6b
Anton Budylin <53855678+aabudilin@users.noreply.github.com>
2020-11-09 13:32:32 +0300

    Добавлен класс

---
+++ GetInstagram.php
@@ -0,0 +1,88 @@
+<?
+
+namespace Standart\GetInstagram;
+
+class GetInstagram {
+  protected $token;
+  protected $path;
+  protected $log;
+
+  public function __construct() {
+	   $this->path = $_SERVER['DOCUMENT_ROOT'].'/php/insta_token.txt';
+	   $this->log = false;
+     $this->token = $this->get_token();
+     $this->chek_token();
+  }
+
+  public function set_token($token) {
+    $this->token = $token;
+  }
+
+  public function get_media() {
+    $url = "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp,thumbnail_url,permalink&access_token=" . $this->token;
+    $instagramCnct = curl_init(); // инициализация cURL подключения
+    curl_setopt($instagramCnct, CURLOPT_URL, $url); // адрес запроса
+    curl_setopt($instagramCnct, CURLOPT_RETURNTRANSFER, 1); // просим вернуть результат
+    $media = json_decode(curl_exec($instagramCnct)); // получаем и декодируем данные из JSON
+    curl_close($instagramCnct); // закрываем соединение
+    return $media;
+  }
+
+  public function get_json() {
+    $url = "https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption,timestamp,thumbnail_url,permalink&access_token=" . $this->token;
+    $instagramCnct = curl_init(); // инициализация cURL подключения
+    curl_setopt($instagramCnct, CURLOPT_URL, $url); // адрес запроса
+    curl_setopt($instagramCnct, CURLOPT_RETURNTRANSFER, 1); // просим вернуть результат
+    $media = curl_exec($instagramCnct); // получаем и декодируем данные из JSON
+    curl_close($instagramCnct); // закрываем соединение
+    return $media;
+  }
+
+  protected function update_token() {
+    $url = "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=" . $this->token;
+    $instagramCnct = curl_init(); // инициализация cURL подключения
+    curl_setopt($instagramCnct, CURLOPT_URL, $url); // адрес запроса
+    curl_setopt($instagramCnct, CURLOPT_RETURNTRANSFER, 1); // просим вернуть результат
+    $response = json_decode(curl_exec($instagramCnct)); // получаем и декодируем данные из JSON
+    curl_close($instagramCnct); // закрываем соединение
+    $accessToken = $response->access_token; // обновленный токен
+	if ($this->log) {
+		echo 'Обновление токена<br />';
+		echo 'Ответ сервера<br /><pre>';
+		print_r($response);
+	}
+    return $accessToken;
+  }
+
+  protected function get_token() {
+    $token = file_get_contents($this->path);
+  	if ($this->log) {
+  		echo 'get_token - '.$token.'<br />';
+  	}
+    return $token;
+  }
+
+  protected function save_token($token) {
+    $result = file_put_contents($this->path,$token);
+  	if ($this->log) {
+  		echo 'save_token - '.$result.'<br />';
+  	}
+  }
+
+  protected function chek_token() {
+    $media = $this->get_media();
+    //Если токен ошибочный обновляем его
+    if (!$media->data) {
+  	  $new_token = $this->update_token();
+  		if ($new_token) {
+        		$this->token = $this->update_token();
+  		    $this->save_token($this->token);
+  		}
+    }
+  }
+
+	public function logged() {
+		$this->log = true;
+	}
+}
+?>
